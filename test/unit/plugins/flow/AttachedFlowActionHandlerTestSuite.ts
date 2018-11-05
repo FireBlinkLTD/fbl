e45a507be66bb5af124a7a41cb5dbd2ce4961e8a
Vlad Tkachenko <vlad@fireblink.com>
2018-11-05 11:12:38 +0200

    #167 #166 Cover new CLI attributes with tests

--- test/unit/plugins/flow/AttachedFlowActionHandlerTestSuite.ts
+++ test/unit/plugins/flow/AttachedFlowActionHandlerTestSuite.ts
@@ -11,7 +11,7 @@ import {IActionHandlerMetadata, IFlowLocationOptions} from '../../../../src/inte
 import {ContextUtil} from '../../../../src/utils';
 import {dirname, join} from 'path';
 import {c} from 'tar';
-import {ChildProcess, fork} from 'child_process';
+import {DummyServerWrapper} from '../../../assets/dummy.http.server.wrapper';
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
@@ -35,117 +35,6 @@ class DummyActionHandler extends ActionHandler {
 
     async execute(options: any, context: any, snapshot: ActionSnapshot): Promise<void> {
         await this.fn(options, context, snapshot);
-    }
-}
-
-interface IDummyServerWrapperConfig {
-    port: number;
-    status?: number;
-    delay?: number;
-    file?: string;
-    ignoreRequest?: boolean;
-    redirectTo?: string;
-}
-
-class DummyServerWrapper {
-    public requestCount = 0;
-
-    private server: ChildProcess | null = null;
-    private onServerClose: Function | null = null;
-    public lastRequest: any;
-
-    constructor(public config: IDummyServerWrapperConfig) {
-        if (this.config.redirectTo) {
-            this.config.status = 302;
-        }
-    }
-
-    private prepareOptions(): string[] {
-        const options = [
-            '-p', this.config.port.toString(),
-            '-s', this.config.status.toString(),
-            '-t', '1'
-        ];
-
-        if (this.config.delay) {
-            options.push('-d', this.config.delay.toString());
-        }
-
-        if (this.config.ignoreRequest) {
-            options.push('--ignore-request');
-        }
-
-        if (this.config.redirectTo) {
-            options.push('-h', JSON.stringify({
-                Location: this.config.redirectTo
-            }));
-        }
-
-        options.push(this.config.file || 'missing_file.txt');
-
-        return options;
-    }
-
-    /**
-     * Start server
-     * @return {Promise<void>}
-     */
-    async start(): Promise<void> {
-        await this.stop();
-
-        const options = this.prepareOptions();
-
-        this.server = fork('dummy.http.server', options, {
-            cwd: join(__dirname, '../../../assets'),
-            silent: true
-        });
-
-        this.server.stdout.on('data', (data) => {
-            console.error(`-> Server.stdout: ${data.toString().trim()}`);
-        });
-
-        this.server.stderr.on('data', (data) => {
-            console.error(`-> Server.stderr: ${data.toString().trim()}`);
-        });
-
-        this.server.on('close', (code, signal) => {
-            console.log('-> Server is stopped. Code: ' + code + '. Signal: ' + signal);
-            this.server = null;
-
-            if (this.onServerClose) {
-                this.onServerClose();
-                this.onServerClose = null;
-            }
-        });
-
-        await new Promise((resolve, reject) => {
-            this.server.on('message', (msg: any) => {
-                if (msg.name === 'onRequest') {
-                    this.requestCount++;
-                    this.lastRequest = msg.payload;
-                }
-
-                if (msg.name === 'started') {
-                    return resolve();
-                }
-
-                if (msg.name === 'failed') {
-                    return reject(new Error('Server failed to start'));
-                }
-            });
-        });
-    }
-
-    async stop(): Promise<void> {
-        if (this.server) {
-            console.log('-> Killing server...');
-
-            // give system time to free port
-            await new Promise(resolve => {
-                this.onServerClose = resolve;
-                this.server.kill('SIGINT');
-            });
-        }
     }
 }
 
